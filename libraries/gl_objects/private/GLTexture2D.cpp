7f3b77f611d473d8128bfbb0061011ee2525ff44
cledant <cledant@student.42.fr>
2020-05-25 00:01:55 +0200

    changed tex2d filtering

--- libraries/gl_objects/private/GLTexture2D.cpp
+++ libraries/gl_objects/private/GLTexture2D.cpp
@@ -84,7 +84,7 @@ GLTexture2D::loadTexture(const char *filepath)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(
       GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 16.0f);
     glGenerateMipmap(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, 0);
